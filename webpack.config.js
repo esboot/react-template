c6711e636b1c1646c0ebbb8f538af74a3d98acfe
webpatch <webpatch@sina.com>
2018-09-27 18:37:19 +0800

    add sass

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,21 @@ module.exports = {
         include: path.resolve(__dirname, 'src'),
         loader: "babel-loader",
       },
+      {
+        test: /\.scss$/,
+        exclude: path.resolve(__dirname, 'src/css/'), // 非src/css下的scss开启局部样式模式
+        loaders: [
+          'style-loader?sourceMap',
+          'css-loader?modules&sourceMap=true&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss-loader?sourceMap',
+          'sass-loader?sourceMap',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src/css/'),
+        loaders: ['style-loader?sourceMap', 'css-loader?sourceMap', 'postcss-loader?sourceMap', 'sass-loader?sourceMap'],
+      },
     ]
   },
   devServer: {
