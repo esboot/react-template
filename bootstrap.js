5db4a9e8a83064aca6fe0ecba3a8b836319c7110
webpatch <webpatch@sina.com>
2018-10-10 17:13:14 +0800

    fix bug

--- bootstrap.js
+++ bootstrap.js
@@ -22,7 +22,7 @@ module.exports = {
   completeMessage: 'To get started:\n\n cd <%=destPath%>\n npm start\n\n',
   filter: {
     'src/model/*': 'redux',
-    'src/store.js': 'redux|router',
+    'src/store.js.ejs': 'redux|router',
   },
   ignore: [
     'src/_part/*',
